b105c4f475e705b7ab502a45ff84f048f925d616
parkcool <yickle1345@naver.com>
2024-09-16 03:53:49 +0000

    Feat: Add color constant

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,36 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    primaryLight: "#E38B2B",
+    primary: "#E07F14",
+    primaryDark: "C97212",
+    primaryLighter: "#FFEBCA",
+
+    greyDark: "#212121",
+    grey: "#424242",
+    greyLight: "#616161",
+    greyLighter: "#F5F5F5",
+
+    white: "#FFFFFF",
+    black: "#000000",
+
+    red: "#F44336",
+    green: "#27AE60",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    primaryLight: "#E38B2B",
+    primary: "#E07F14",
+    primaryDark: "C97212",
+    primaryLighter: "#FFEBCA",
+
+    greyDark: "#212121",
+    grey: "#424242",
+    greyLight: "#616161",
+    greyLighter: "#F5F5F5",
+
+    white: "#FFFFFF",
+    black: "#000000",
+
+    red: "#F44336",
+    green: "#27AE60",
   },
 };
