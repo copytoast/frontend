4235365af74c5954275af9e69a39f9dec04189be
parkcool <yickle1345@naver.com>
2024-10-06 03:35:16 +0000

    Feat: Implement `Toast` component

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,16 +6,26 @@ import ColumnFlex from "@/components/ColumnFlex";
 import Section from "@/components/Section";
 
 import GreyLogo from "@/assets/vectors/logo_grey.svg";
+import Toast from "@/components/Toast";
 
 export default function Home() {
   return (
     <ColumnFlex style={styles.root}>
       <Section
-        title="암기빵"
+        title="내가 담은 암기빵"
         titleIcon={<GreyLogo width={20} height={20} />}
         titleArrowVisible
       >
-        <ColumnFlex />
+        <ColumnFlex style={styles.sectionContent}>
+          <Toast
+            name="수능 영단어 1,000선"
+            description="대학수학능력시험 영어 영역에 빈출되는 영단어 1,000개를 모아놓았어요."
+            like={10}
+            added
+            my
+            detailButtonVisible
+          />
+        </ColumnFlex>
       </Section>
     </ColumnFlex>
   );
@@ -25,5 +35,9 @@ const styles = StyleSheet.create({
   root: {
     paddingVertical: 20,
     paddingHorizontal: 10,
+  },
+  sectionContent: {
+    padding: 5,
+    paddingTop: 0,
   },
 });
---
+++ components/Toast.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  View,
+  type StyleProp,
+  type ViewStyle,
+} from "react-native";
+
+import Typography from "@/components/Typography";
+import RowFlex from "@/components/RowFlex";
+import ToastIcon from "@/components/ToastIcon";
+import ColumnFlex from "@/components/ColumnFlex";
+
+import Colors from "@/constants/Colors";
+
+import MoreIcon from "@/assets/vectors/more.svg";
+
+interface ToastProps {
+  name: string;
+  description: string;
+  like: number;
+  added: boolean;
+  checkBoxVisible?: boolean;
+  onCheckChange?: (checked: boolean) => void;
+  my?: boolean;
+  picture?: string;
+  detailButtonVisible?: boolean;
+  style?: StyleProp<ViewStyle>;
+}
+
+function Toast({
+  name,
+  description,
+  like,
+  added,
+  checkBoxVisible,
+  onCheckChange,
+  my,
+  picture,
+  detailButtonVisible,
+  style,
+}: ToastProps) {
+  return (
+    <RowFlex style={[styles.root, style]} gap={10}>
+      <RowFlex style={styles.left} gap={10}>
+        <ToastIcon size={40} picture={picture} like={{ count: like, added }} />
+        <ColumnFlex style={styles.info}>
+          <RowFlex style={styles.title} gap={5}>
+            <Typography size={16} weight="bold" color={Colors.greyDark}>
+              {name}
+            </Typography>
+            {my && (
+              <View style={styles.my}>
+                <Typography size={10} color={Colors.white} weight={"bold"}>
+                  MY
+                </Typography>
+              </View>
+            )}
+          </RowFlex>
+          <Typography
+            size={14}
+            weight="regular"
+            numberOfLines={1}
+            color={Colors.greyLight}
+          >
+            {description}
+          </Typography>
+        </ColumnFlex>
+      </RowFlex>
+      {detailButtonVisible && (
+        <Pressable>
+          <MoreIcon width={24} height={24} />
+        </Pressable>
+      )}
+    </RowFlex>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: "space-between",
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: Colors.white,
+  },
+  left: {
+    alignItems: "center",
+    flex: 1,
+  },
+  title: {
+    alignItems: "center",
+  },
+  info: {
+    flex: 1,
+  },
+  my: {
+    paddingHorizontal: 6,
+    borderRadius: 10,
+    height: 18,
+    backgroundColor: Colors.primary,
+    justifyContent: "center",
+  },
+});
+
+export default Toast;
