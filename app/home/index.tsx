70a549ca9b17fdae7855b0ef0ae63ef4de7a44cd
parkcool <yickle1345@naver.com>
2024-10-09 05:56:34 +0000

    Feat: Add quick buttons in home

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,10 +1,15 @@
 import { StatusBar, StyleSheet } from "react-native";
 
+import GreyLogo from "@/assets/vectors/logo_grey.svg";
+import AddIcon from "@/assets/vectors/add.svg";
+import ListIcon from "@/assets/vectors/list.svg";
+
 import ColumnFlex from "@/components/ColumnFlex";
 import Section from "@/components/Section";
-
-import GreyLogo from "@/assets/vectors/logo_grey.svg";
 import Toast from "@/components/Toast";
+import RowFlex from "@/components/RowFlex";
+import Button from "@/components/Button";
+import Colors from "@/constants/Colors";
 
 export default function Home() {
   return (
@@ -25,6 +30,28 @@ export default function Home() {
             detailButtonVisible
           />
         </ColumnFlex>
+        <RowFlex gap={10} style={styles.quickButtonContainer}>
+          <Button
+            label={`새 암기빵\n만들기`}
+            icon={<AddIcon width={20} height={20} />}
+            iconPosition={"right"}
+            color={Colors.greyLighter}
+            style={styles.quickButton}
+            contentStyle={styles.quickButtonContent}
+            fontSize={16}
+            fontWeight={"medium"}
+          />
+          <Button
+            label={`다른 암기빵\n둘러보기`}
+            icon={<ListIcon width={20} height={20} />}
+            iconPosition={"right"}
+            color={Colors.greyLighter}
+            style={styles.quickButton}
+            contentStyle={styles.quickButtonContent}
+            fontSize={16}
+            fontWeight={"medium"}
+          />
+        </RowFlex>
       </Section>
     </ColumnFlex>
   );
@@ -38,5 +65,17 @@ const styles = StyleSheet.create({
   sectionContent: {
     padding: 5,
     paddingTop: 0,
+  },
+  quickButtonContainer: {
+    padding: 10,
+  },
+  quickButton: {
+    flex: 1,
+  },
+  quickButtonContent: {
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    borderRadius: 10,
+    padding: 15,
   },
 });
